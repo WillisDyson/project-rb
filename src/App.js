3c34f5dcbeb62579a12a07945aa623b6b8a3a6b9
Willis Dyson <willisdyson@gmail.comD>
2025-07-18 15:52:08 +0100

    Setting up React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Project RB</h1>
+      <p>More info coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
