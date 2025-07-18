506a5b77d85bdb3a944204a1a0f7fd77db48fa28
Willis Dyson <willisdyson@gmail.comD>
2025-07-18 16:58:38 +0100

    Logo and navbar setup

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,12 +1,16 @@
 import styles from './NavBar.module.scss';
-
-
+import NavDropdown from './nav-dropdown/NavDropdown';
+import Logo from '../global/logo/Logo';
 
 const NavBar = () => {
 
-
     return (
-        <nav className={styles.nav}>Hello SCSS</nav>
+        <nav className={styles.nav}>
+            <NavDropdown />
+            <Logo />
+            <NavDropdown />
+            {/* <ContactButton /> */}
+        </nav>
     )
 }
 
