440cbd8589346f0f0c94afeeb87adf5f222834cb
Willis Dyson <willisdyson@gmail.com>
2026-01-02 11:00:49 +0000

    Footer legal and social links WIP

--- src/components/footer/footer-links/FooterLinks.js
+++ src/components/footer/footer-links/FooterLinks.js
@@ -1,15 +1,34 @@
 import styles from "./FooterLinks.module.scss";
+import { legalLinksData, socialLinksData } from "data/footer/footerData";
 
 const FooterLinks = ({ className, variant }) => {
   return (
     <div className={`${styles["footer-links"]} ${className ? className : ""} ${
         variant ? styles[variant] : ""}`}>
+      <div className={styles["footer-links__inner"]}>
         <div className={styles["footer-links__column"]}>
-          <h3 className={styles["footer-links__header"]}>Legal stuff</h3>
+          <h2 className={styles["footer-links__header"]}>{legalLinksData.headerText}</h2>
           <ul className={styles["footer-links__list"]}>
-            <li className={styles["footer-links__item"]}><a href="/about" className={styles["footer-links__link"]}>Terms & Conditions</a></li>
+            {legalLinksData.links.map((link) => (
+              <li key={`${link.href}-${link.text}`} className={styles["footer-links__item"]}>
+                <a href={link.href} className={styles["footer-links__link"]}>{link.text}</a>
+              </li>
+            ))}
           </ul>
         </div>
+        <div className={styles["footer-links__column"]}>
+          <h2 className={styles["footer-links__header"]}>{socialLinksData.headerText}</h2>
+          <ul className={styles["footer-links__list"]}>
+            {socialLinksData.links.map((link) => (
+              <li key={`${link.href}-${link.alt}`} className={styles["footer-links__item"]}>
+                <a href={link.href} className={styles["footer-links__link"]}>
+                  <img src={link.imageUrl} alt={link.alt} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/data/footer/footerData.js
+++ src/data/footer/footerData.js
@@ -3,3 +3,21 @@ export const subscribeToNewsletterData = {
   buttonUrl: "#",
   buttonText: "Subscribe to our free newsletter",
 };
+
+export const legalLinksData = {
+  headerText: "Legal stuff",
+  links: [
+    { text: "Terms & Conditions", href: "#"},
+    { text: "Privacy Policy", href: "#" },
+    { text: "Cookie Policy", href: "#" },
+  ],
+};
+
+export const socialLinksData = {
+  headerText: "Find us on social media",
+  links: [
+    { imageUrl: "/", href: "#", alt: "Facebook" },
+    { imageUrl: "/", href: "#", alt: "X" },
+    { imageUrl: "/", href: "#", alt: "LinkedIn" },
+  ],
+};
